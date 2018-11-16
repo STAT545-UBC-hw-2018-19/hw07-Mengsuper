b3f3918633726b0d340b51afd639b33154648ab8
Meng Li <meng@phas.ubc.ca>
2018-11-15 22:49:32 -0800

    update

--- tests/testthat/test_exp.R
+++ tests/testthat/test_exp.R
@@ -1,4 +1,4 @@
-context("logarithm functions")
+context("exponential function")
 
 test_that("Input is not numbers", {
     x <- c("Hello", "World")
@@ -7,6 +7,11 @@ test_that("Input is not numbers", {
 
 test_that("At least numeric values work.", {
     x <- c(-10, 1, 2, 3)
+    expect_equal(expo(x), exp(x))
+})
+
+test_that("At least numeric values work.", {
+    x <- c(-0.1, 100, -100, 3.5)
     expect_equal(expo(x), exp(x))
 })
 
